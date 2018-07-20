8f8e6a7bbdd39ee2c294a1db1426477f3f2d1138
Mikolaj Walczak <mwalczak@fb.com>
2018-07-20 08:31:53 -0700

    Add safety check for the request type in dns executor

--- dns/dns.go
+++ dns/dns.go
@@ -1,12 +1,14 @@
 package dns
 
 import (
+	"fmt"
+
 	"github.com/miekg/dns"
 	"github.com/pinterest/bender"
 )
 
 // ResponseValidator validates a DNS response.
-type ResponseValidator func(request interface{}, response *dns.Msg) error
+type ResponseValidator func(request, response *dns.Msg) error
 
 // CreateExecutor creates a new DNS RequestExecutor.
 func CreateExecutor(client *dns.Client, responseValidator ResponseValidator, hosts ...string) bender.RequestExecutor {
@@ -16,14 +18,17 @@ func CreateExecutor(client *dns.Client, responseValidator ResponseValidator, hos
 
 	var i int
 	return func(_ int64, request interface{}) (interface{}, error) {
-		msg := request.(*dns.Msg)
+		msg, ok := request.(*dns.Msg)
+		if !ok {
+			return nil, fmt.Errorf("invalid request type %T, want: *dns.Msg", request)
+		}
 		addr := hosts[i]
 		i = (i + 1) % len(hosts)
 		resp, _, err := client.Exchange(msg, addr)
 		if err != nil {
 			return nil, err
 		}
-		if err = responseValidator(request, resp); err != nil {
+		if err = responseValidator(msg, resp); err != nil {
 			return nil, err
 		}
 		return resp, nil
